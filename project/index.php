64633cb8313632ce9de06bb1da40cf4648db739a
artemihx <art.pozx1@gmail.com>
2024-10-24 13:42:26 +0700

    tasks

--- project/index.php
+++ project/index.php
@@ -1,1 +1,35 @@
-<?php phpinfo()?>
+<?php
+function printOwnCode() {
+    $code = file_get_contents(__FILE__);
+    echo htmlspecialchars($code);
+}
+
+function listAllFiles() {
+    $files = scandir(__DIR__);
+    foreach ($files as $file) {
+        if ($file !== '.' && $file !== '..') {
+            echo $file . PHP_EOL;
+        }
+    }
+}
+
+function listDirectories() {
+    $files = scandir(__DIR__);
+    foreach ($files as $file) {
+        if ($file !== '.' && $file !== '..' && is_dir($file)) {
+            echo $file . PHP_EOL;
+        }
+    }
+}
+
+echo "Собственный код программы:" . PHP_EOL . '<br>';
+printOwnCode();
+echo PHP_EOL;
+
+echo '<br>' ."Список всех файлов в текущей директории:" . PHP_EOL . '<br>';
+listAllFiles();
+echo PHP_EOL;
+
+echo '<br>' . "Список только папок в текущей директории:" . PHP_EOL . '<br>';
+listDirectories();
+?>
