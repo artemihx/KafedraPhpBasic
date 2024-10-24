39e3e47f1736d0a6a13b8774230e7ade2d62ac9d
artemihx <art.pozx1@gmail.com>
2024-10-24 13:23:56 +0700

    tasks

--- project/index.php
+++ project/index.php
@@ -1,1 +1,19 @@
-<?php phpinfo()?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Форма входа</title>
+</head>
+<body>
+<form action="/login.php" method="post">
+    <label>
+        Логин <input type="text" name="login" required>
+    </label>
+    <br>
+    <label>
+        Пароль <input type="password" name="password" required>
+    </label>
+    <br>
+    <input type="submit" value="Войти">
+</form>
+</body>
+</html>
